26e8ff9ccedb26328a11481592aaac86510a3519
silviu.marin <marin.silviu11@yahoo.com>
2017-07-11 18:08:01 +0300

    1

---
+++ SQL/learnSQL/src/main/java/com/teamnet/sqlproject/App.java
@@ -0,0 +1,13 @@
+package com.teamnet.sqlproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
